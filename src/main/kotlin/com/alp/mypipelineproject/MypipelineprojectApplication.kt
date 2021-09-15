cfef79243e57c4c196246794e0246b06cf1a0694
apeterson <apeterson@entegral.com>
2021-09-15 13:13:29 -0500

    First commit

---
+++ src/main/kotlin/com/alp/mypipelineproject/MypipelineprojectApplication.kt
@@ -0,0 +1,11 @@
+package com.alp.mypipelineproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MypipelineprojectApplication
+
+fun main(args: Array<String>) {
+	runApplication<MypipelineprojectApplication>(*args)
+}
---
+++ src/test/kotlin/com/alp/mypipelineproject/MypipelineprojectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.alp.mypipelineproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MypipelineprojectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
